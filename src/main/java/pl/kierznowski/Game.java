4ff2a926cd9e038a390267eda79c5df69d91051b
bessekratte <34283405+bessekratte@users.noreply.github.com>
2019-07-31 19:18:50 +0200

    enable hot swapping templates (just F5 to reload content)

--- src/main/java/pl/kierznowski/Game.java
+++ src/main/java/pl/kierznowski/Game.java
@@ -1,47 +1,40 @@
 package pl.kierznowski;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 
 @SpringBootApplication
 public class Game {
 
     @Autowired
-    private SpringResourceTemplateResolver templateResolver;
+    private ThymeleafProperties properties;
 
-    private ApplicationContext appContext;
+    @Value("${spring.thymeleaf.templates_root:}")
+    private String templatesRoot;
+
 
     public static void main(String[] args) {
         SpringApplication.run(Game.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner thymeleafSettings() {
-//        System.out.println("im here");
-//        return args -> templateResolver.setCacheable(false);
-//    }
-
-
     @Bean
-    public SpringResourceTemplateResolver templateResolver(){
-        // SpringResourceTemplateResolver automatically integrates with Spring's own
-        // resource resolution infrastructure, which is highly recommended.
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(this.appContext);
-        templateResolver.setPrefix("classpath:/templates/");
-        templateResolver.setSuffix(".html");
-        // HTML is the default value, added here for the sake of clarity.
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        // Template cache is true by default. Set to false if you want
-        // templates to be automatically updated when modified.
-        templateResolver.setCacheable(true);
-        return templateResolver;
+    public ITemplateResolver defaultTemplateResolver() {
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setSuffix(properties.getSuffix());
+        resolver.setPrefix(templatesRoot);
+        resolver.setTemplateMode(properties.getMode());
+        resolver.setCacheable(properties.isCache());
+        return resolver;
     }
 }
